5b65ea9e184ee665ff368d6cc1bc1073e21f5793
DavideTotaro04 <DaviTotaro92@gmail.com>
2024-12-03 13:38:18 +0100

    Movimento e collisioni
    movimento solo sull'asse x e collisioni beta

---
+++ Assets/collision.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class collision
+{
+    private void OnCollisionEnter(Collision coll)
+{
+	if(coll.relativeVelocity.magnitude >= 10f)
+	{
+		
+	}
+}
+}
--- Assets/movment.cs
+++ Assets/movment.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class movment : MonoBehaviour
+/*public class movment : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,4 +13,24 @@ public class movment : MonoBehaviour
     {
         
     }
+}*/
+
+public class NuovoScript : MonoBehaviour
+{
+    public Vector3 movimento;
+
+    void Start()
+    {
+    
+    }
+
+     void FixedUpdate()
+    {
+        if(movimento.x<10)
+
+        movimento.x+=0.1f;
+
+        transform.position = movimento;
+    }
+
 }
